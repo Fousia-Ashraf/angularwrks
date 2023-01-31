b5faeed7fc650649f3d6f769304116f88a2bc254
unknown <you@example.com>
2023-02-01 00:23:22 +0530

    second

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,16 +14,16 @@ pholder="Account number"
 
 // acno=''       =initialising method
 //or
-acno:any
-pass:any
+// acno:any
+// pass:any
 
 
 UserDetails:any={
   1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-  1001:{acno:1001,username:"fousi",password:"abc456",balance:0},
-  1002:{acno:1002,username:"shani",password:"abc789",balance:0},
-  1003:{acno:1003,username:"akku",password:"abc101",balance:0},
-  1004:{acno:1004,username:"sharfi",password:"abc112",balance:0}
+  1001:{acno:1001,username:"fousi",password:"abc123",balance:0},
+  1002:{acno:1002,username:"shani",password:"abc123",balance:0},
+  1003:{acno:1003,username:"akku",password:"abc123",balance:0},
+  1004:{acno:1004,username:"sharfi",password:"abc123",balance:0}
 
 
 }
@@ -33,21 +33,59 @@ UserDetails:any={
 ngOnInit(): void{
 
 }
-login(){
-  alert('login clicked')
+// login(){
+//   // alert('login clicked')
+// var acnum=this.acno
+// var psw=this.pass
+// var UserDetails=this.UserDetails
+// if(acnum in UserDetails){
+// if(psw==UserDetails[acnum]['password']){
+// alert('login success')
+// }
+// else{
+//   alert('incorrect password')
+// }
+// }
+// else{
+//   alert("acnum incorrect or not registered yet")
+// }
+
+
+// }
+
+login(a:any,b:any){
+  console.log(a.value);
+
   
+  // alert('login clicked')
+var acnum=a.value
+var psw=b.value
+var UserDetails=this.UserDetails
+if(acnum in UserDetails){
+if(psw==UserDetails[acnum]['password']){
+alert('login success')
 }
-acnoChange(event:any){
-  this.acno=event.target.value
-  console.log(this.acno);
-  
+else{
+  alert('incorrect password')
 }
-PassChange(event:any){
-  this.pass=event.target.value
-console.log(this.pass);
+}
+else{
+  alert("acnum incorrect or not registered yet")
+}
 
 
 }
+// acnoChange(event:any){
+//   this.acno=event.target.value
+//   // console.log(this.acno);
+  
+// }
+// PassChange(event:any){
+//   this.pass=event.target.value
+// // console.log(this.pass);
+
+
+// }
   } 
 
 
